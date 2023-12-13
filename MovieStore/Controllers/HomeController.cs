d4f642e79dab5582fce4393abe5651396d7bb704
Nour-Baklouti <nourbaklouti2022@gmail.com>
2023-12-13 21:36:11 +0100

    nour et rania commit

---
+++ MovieStore/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Repositories.Abstract;
+using MovieStore.Models;
+using System.Diagnostics;
+
+
+namespace MovieStoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMovieService _movieService;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(IMovieService movieService, ILogger<HomeController> logger)
+        {
+            _movieService = movieService;
+            _logger = logger;
+        }
+        public IActionResult Index(string term = "", int currentPage = 1)
+        {
+            var movies = _movieService.List(term, true, currentPage);
+            return View(movies);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult MovieDetail(int movieId)
+        {
+            var movie = _movieService.GetById(movieId);
+            return View(movie);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
---
+++ MovieStore/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using MovieStore.Models.DTO;
+
+namespace MovieStore.Repositories.Abstract
+{
+    /*public interface IUserAuthenticationService
+    {
+        Task<Status> LoginAsync(LoginModel model);
+        Task LogoutAsync();
+        Task<Status> RegisterAsync(RegistrationModel model);
+        //Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username);
+    }*/
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
